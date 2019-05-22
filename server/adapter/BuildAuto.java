638b26703d3ca5589a2be24c1c571c45ae67a75a
ashleyylimmm <31688457+ashleyylimmm@users.noreply.github.com>
2019-05-21 17:23:26 -0700

    Added files via upload

---
+++ server/adapter/BuildAuto.java
@@ -0,0 +1,14 @@
+/**
+ *	BuildAuto.java
+ *	Empty class that extends ProxyAutomobile for API reasons
+ *	
+ *	Eclipse Neon.2 Release (4.6.2), macOS Sierra
+ *	Java SE 8 [1.8.0_45]
+ **	@author Ashley Lim
+ *	@version June 2018
+ */
+package adapter;
+
+public class BuildAuto extends ProxyAutomobile implements CreateAuto, UpdateAuto, FixAuto, EditAuto, ProxyLink, server.AutoServer{
+	
+}
---
+++ server/adapter/CreateAuto.java
@@ -0,0 +1,16 @@
+/**
+ *	CreateAuto.java
+ *	Write a description of your file here
+ *	
+ *	Eclipse Neon.2 Release (4.6.2), macOS Sierra
+ *	Java SE 8 [1.8.0_45]
+ **	@author Ashley Lim
+ *	@version June 2018
+ */
+package adapter;
+import java.util.ArrayList;
+public interface CreateAuto {
+	public void buildAuto(Object o, String type);
+	public String printAuto(String key);
+	public ArrayList<String> getModelList();
+}
